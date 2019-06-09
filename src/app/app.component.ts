51f01b3d8f99eeb97998a5d58541de47f9d802ac
JavierMachucaA <javier.machuca.a@gmail.com>
2019-06-09 12:41:57 -0400

    agregan pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,7 @@ export class AppComponent {
   title = 'pipes';
   nombre = "Javier Machuca" ;
   arreglo : number[] = [1,2,3,4,5,6,7,8,9,10];
+  PI : number = Math.PI;
+  a:number = 0.234;
+  value : number =  120456.123;
 }
